651a732d7ecf3fd7c60a64bb55e3b35f0f69508f
Kayardurdass <angladeulysse@gmail.com>
2022-02-24 00:33:41 +0100

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/system.hpp>
+#include <SFML/Network.hpp>
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(640, 480), "My first sfml window", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event ev;
+
+	while (window.isOpen())
+	{
+		//Event Polling
+		while (window.pollEvent(ev)) 
+		{
+
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+					window.close();
+				break;
+			}
+		}
+
+		//Update
+
+		//Render
+		window.clear();  //clear old frame
+
+
+		window.display();  //draw the new frame
+	}
+
+}
