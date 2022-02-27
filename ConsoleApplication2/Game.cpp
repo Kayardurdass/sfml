d39be7854809e0e1596d410a52907725ac78ecf0
Kayardurdass <angladeulysse@gmail.com>
2022-02-27 23:40:34 +0100

    Adding player score UI and player collisions

--- ConsoleApplication2/Game.cpp
+++ ConsoleApplication2/Game.cpp
@@ -4,24 +4,36 @@
 void Game::InitVariables()
 {
 	this->window = nullptr;
+
+	//game Logic
+	this->points = 0;
+	this->enemySpawnTimer = this->enemySpawnTimerMax;
+	this->enemySpawnTimerMax = 40.f;
+	this->maxEnemies = 10;
+	this->score = 0;
 }
 
 void Game::InitWindow()
 {
 	this->videoMode.height = 600;
-	this->videoMode.width = 800;
-	this->window = new sf::RenderWindow(this->videoMode, "My first sfml window", sf::Style::Titlebar | sf::Style::Close);
+	this->videoMode.width = 400;
+	this->window = new sf::RenderWindow(this->videoMode, "ShittyGame", sf::Style::Titlebar | sf::Style::Close);
 
-	this->window->setFramerateLimit(144);
+	this->window->setFramerateLimit(60);
 }
 
 void Game::InitEnemies()
 {
-	this->enemy.setPosition(sf::Vector2f(100.f, 100.f));
-	this->enemy.setSize(sf::Vector2f(100.f, 100.f));
+	this->enemy.setPosition(sf::Vector2f(10.f, 10.f));
+	this->enemy.setSize(sf::Vector2f(50.f, 50.f));
 	this->enemy.setFillColor(sf::Color::Cyan);
-	this->enemy.setOutlineColor(sf::Color::Red);
-	this->enemy.setOutlineThickness(1.f);
+}
+
+void Game::InitPlayer()
+{
+	this->playerBody.setPosition(sf::Vector2f((this->window->getSize().x/2), (this->window->getSize().y) - 10));
+	this->playerBody.setSize(sf::Vector2f(50.f, 50.f));
+	this->playerBody.setFillColor(sf::Color::Green);
 }
 
 //Constructors / Destructors
@@ -30,6 +42,7 @@ Game::Game()
 	this->InitVariables();
 	this->InitWindow();
 	this->InitEnemies();
+	this->InitPlayer();
 }
 
 Game::~Game() 
@@ -42,8 +55,15 @@ const bool Game::GetWindowOpen() const
 	return this->window->isOpen();
 }
 
-//Accessors
+void Game::SpawnEnemy()
+{
+	this->enemy.setPosition(
+		static_cast<float>(rand() % static_cast<int>(this->window->getSize().x - this->enemy.getSize().x)), 0.f);
 
+	this->enemy.setFillColor(sf::Color::Green);
+
+	this->enemies.push_back(this->enemy);
+}
 
 void Game::PollEvents()
 {
@@ -56,6 +76,10 @@ void Game::PollEvents()
 		case sf::Event::KeyPressed:
 			if (this->ev.key.code == sf::Keyboard::Escape)
 				this->window->close();
+			if (this->ev.key.code == sf::Keyboard::D)
+				this->xPos += 10.f;
+			if (this->ev.key.code == sf::Keyboard::Q)
+				this->xPos -= 10.f;
 		}
 	}
 }
@@ -64,6 +88,58 @@ void Game::PollEvents()
 void Game::Update()
 {
 	this->PollEvents();
+
+	this->UpdatePlayer();
+
+	this->UpdateEnemies();
+	
+}
+
+void Game::UpdateEnemies()
+{ 
+	//Update the timer
+	if (this->enemies.size() < this->maxEnemies) {
+		if (this->enemySpawnTimer >= this->enemySpawnTimerMax) {
+			//spawns the enemy
+			this->SpawnEnemy();
+			this->enemySpawnTimer = 0.f;
+		}
+		else {
+			this->enemySpawnTimer += 1.f;
+		}
+	}
+
+	//Move the enemies
+	for (int i = 0; i < this->enemies.size(); i++) {
+
+		bool deleted = false;
+
+		this->enemies[i].move(0.f, 3.f);
+
+		if (this->enemies[i].getPosition().y > this->window->getSize().y) {
+			this->score -= 1;
+			deleted = true;
+		}
+		if (this->playerBody.getGlobalBounds().intersects(this->enemies[i].getGlobalBounds())) {
+			this->score += 1;
+			deleted = true;
+		}
+
+		if (deleted) {
+			this->enemies.erase(this->enemies.begin() + i);
+		}
+	}
+}
+
+void Game::UpdatePlayer()
+{
+	playerBody.setPosition(xPos, this->playerBody.getPosition().y);
+	std::cout << to_string(xPos) << "\n";
+}
+
+void Game::UpdateVariables()
+{
+	this->enemySpawnTimerMax -= score;
 }
 
 void Game::Render()
@@ -71,8 +147,39 @@ void Game::Render()
 	//clear the window
 	this->window->clear();
 
-	this->window->draw(this->enemy);
+	this->RenderEnemies();
+
+	this->RenderPlayer();
+
+	this->renderUI();
 
 	//Update and display the things
 	this->window->display();
+}
+
+void Game::RenderEnemies()
+{
+	//rendering all the enemies
+
+	for (auto &e : this->enemies) {
+		this->window->draw(e);
+	}
+}
+
+void Game::RenderPlayer()
+{
+	this->window->draw(this->playerBody);
+}
+
+void Game::renderUI()
+{
+	sf::Font font;
+	font.loadFromFile("C:/Users/Admin/AppData/Local/Temp/Rar$DRa13708.15504/arial.ttf");
+	// Create a text
+	sf::Text scoreText(to_string(this->score), font);
+	scoreText.setCharacterSize(30);
+	scoreText.setStyle(sf::Text::Bold);
+	scoreText.setFillColor(sf::Color::Red);
+
+	this->window->draw(scoreText);
 }
--- ConsoleApplication2/Game.h
+++ ConsoleApplication2/Game.h
@@ -1,4 +1,10 @@
 #pragma once
+using namespace std;
+
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <string>
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
@@ -14,13 +20,26 @@ private:
 	sf::VideoMode videoMode;
 	sf::Event ev;
 
+	//Player
+	float xPos;
+	sf::RectangleShape playerBody;
+
+	//game Logic
+	int points;
+	float enemySpawnTimer;
+	float enemySpawnTimerMax;
+	int maxEnemies;
+	int score;
+
 	//GameObjects
+	std::vector < sf::RectangleShape> enemies;
 	sf::RectangleShape enemy;
 
 	//private Variables
 	void InitVariables();
 	void InitWindow();
 	void InitEnemies();
+	void InitPlayer();
 public:
 	//Constructors / Destructors
 	Game();
@@ -30,8 +49,15 @@ public:
 	const bool GetWindowOpen() const;
 
 	// Functions
+	void SpawnEnemy();
 	void PollEvents();
 	void Update();
+	void UpdateEnemies();
+	void UpdatePlayer();
+	void UpdateVariables();
 	void Render();
+	void RenderEnemies();
+	void RenderPlayer();
+	void renderUI();
 };
 
